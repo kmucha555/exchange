57a4330c564548a037e6049ab8af4e89bece2289
kmucha555 <Cfki83vm7>
2020-03-05 12:54:31 +0100

    Migrating TransactionModel fields to non-final.

--- src/main/java/pl/mkjb/exchange/model/TransactionModel.java
+++ src/main/java/pl/mkjb/exchange/model/TransactionModel.java
@@ -1,9 +1,9 @@
 package pl.mkjb.exchange.model;
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import pl.mkjb.exchange.util.TransactionTypeConstant;
 import pl.mkjb.exchange.validator.TransactionAmount;
 import pl.mkjb.exchange.validator.TransactionType;
@@ -12,26 +12,23 @@ import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.util.UUID;
 
-@JsonDeserialize(builder = TransactionModel.TransactionModelBuilder.class)
-@Builder(builderClassName = "TransactionModelBuilder", toBuilder = true)
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
 @TransactionAmount(transactionAmountFieldName = "transactionAmount")
 public class TransactionModel {
-    private final UUID currencyRateId;
-    private final String currencyCode;
-    private final BigDecimal currencyUnit;
-    private final BigDecimal transactionPrice;
-    private final BigDecimal userWalletAmount;
+    private UUID currencyRateId;
+    private String currencyCode;
+    private BigDecimal currencyUnit;
+    private BigDecimal transactionPrice;
+    private BigDecimal userWalletAmount;
 
     @NotNull
-    private final BigDecimal transactionAmount;
+    private BigDecimal transactionAmount;
 
-    private final BigDecimal maxAllowedTransactionAmount;
+    private BigDecimal maxAllowedTransactionAmount;
 
     @TransactionType
-    private final TransactionTypeConstant transactionTypeConstant;
-
-    @JsonPOJOBuilder(withPrefix = "")
-    public static class TransactionModelBuilder {
-    }
+    private TransactionTypeConstant transactionTypeConstant;
 }
