3b9d13d97cb2bf94b173af35b0d52ca2a54a467b
kmucha555 <Cfki83vm7>
2020-03-05 15:27:19 +0100

    Code refactor

--- src/main/java/pl/mkjb/exchange/validator/TransactionAmountValidator.java
+++ src/main/java/pl/mkjb/exchange/validator/TransactionAmountValidator.java
@@ -12,6 +12,7 @@ import pl.mkjb.exchange.service.TransactionFacadeService;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 import java.math.BigDecimal;
+import java.util.function.Predicate;
 
 @RequiredArgsConstructor
 public class TransactionAmountValidator implements ConstraintValidator<TransactionAmount, TransactionModel> {
@@ -26,21 +27,10 @@ public class TransactionAmountValidator implements ConstraintValidator<Transacti
 
     @Override
     public boolean isValid(TransactionModel transactionModel, ConstraintValidatorContext context) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        val customUser = (CustomUser) authentication.getPrincipal();
-
-        val transactionType = transactionModel.getTransactionTypeConstant();
-
-        val currencyRateEntity = currencyService.findCurrencyRateByCurrencyRateId(transactionModel.getCurrencyRateId());
-        val currencyUnit = currencyRateEntity.getCurrencyEntity().getUnit();
-        val buyAmount = transactionModel.getTransactionAmount();
-        val maxTransactionAmount = transactionFacadeService.estimateMaxTransactionAmount()
-                .apply(transactionType)
-                .apply(currencyRateEntity, customUser);
-
-        val isValid = buyAmount.compareTo(BigDecimal.ZERO) > 0 &&
-                buyAmount.remainder(currencyUnit).compareTo(BigDecimal.ZERO) == 0 &&
-                buyAmount.compareTo(maxTransactionAmount) < 1;
+        val isValid = isTransactionAmountGreaterThenZero()
+                .and(isTransactionAmountDivisibleByCurrencyUnit())
+                .and(isTransactionAmountLessThenFundsAvailableInExchange())
+                .test(transactionModel);
 
         if (!isValid) {
             context.disableDefaultConstraintViolation();
@@ -52,5 +42,31 @@ public class TransactionAmountValidator implements ConstraintValidator<Transacti
         }
 
         return isValid;
+    }
+
+    private Predicate<TransactionModel> isTransactionAmountGreaterThenZero() {
+        return transactionModel -> transactionModel.getTransactionAmount().compareTo(BigDecimal.ZERO) > 0;
+    }
+
+    private Predicate<TransactionModel> isTransactionAmountDivisibleByCurrencyUnit() {
+        return transactionModel -> {
+            val currencyUnit = currencyService.findCurrencyRateByCurrencyRateId(transactionModel.getCurrencyRateId())
+                    .getCurrencyEntity()
+                    .getUnit();
+            val transactionAmount = transactionModel.getTransactionAmount();
+            return transactionAmount.remainder(currencyUnit).compareTo(BigDecimal.ZERO) == 0;
+        };
+    }
+
+    private Predicate<TransactionModel> isTransactionAmountLessThenFundsAvailableInExchange() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        val customUser = (CustomUser) authentication.getPrincipal();
+        return transactionModel -> {
+            val currencyRateEntity = currencyService.findCurrencyRateByCurrencyRateId(transactionModel.getCurrencyRateId());
+            val maxTransactionAmount = transactionFacadeService.estimateMaxTransactionAmount()
+                    .apply(transactionModel.getTransactionTypeConstant())
+                    .apply(currencyRateEntity, customUser);
+            return transactionModel.getTransactionAmount().compareTo(maxTransactionAmount) < 1;
+        };
     }
 }
